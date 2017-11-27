8ed3a366fa7c10b71cef0a2263137f932c11beea
SCHARKSPC\daniel <daniel.tiarks@gmail.com>
2017-11-27 19:23:48 +0100

    Added calculation for binding energy of trilobites

--- Chap5/storage.py
+++ Chap5/storage.py
@@ -18,6 +18,9 @@ def decay(t,A,tau,f,V):
 #*np.exp(-t/tau2)  
 def decay2(t,A,tau):
   return A*np.exp(-2*t**2/tau**2)
+  
+def trilobite_decay(t,alpha,omega,tau,A0):
+    return A0*np.exp(-t**1/(tau**1))*(np.cos(alpha)**4+np.sin(alpha)**4+2*np.cos(alpha)**2*np.sin(alpha)**2*np.cos(omega*t))
 
 
 tauTh=11.7
@@ -26,13 +29,20 @@ store_dir="store_retr"
   
 data1=np.loadtxt(os.path.join(store_dir,"store_001.tsv"))
 
-popt1=[0.2,5,0.25,0.4]
-popt1, pcov1 = curve_fit(decay, data1[:,0],1*(data1[:,1]), popt1)
+data1[:,0]=data1[:,0]+0.15
+
+#popt1=[0.2,5,0.25,0.4]
+popt1=[0.5,2*np.pi*0.2,5.,0.2]
+popt1, pcov1 = curve_fit(trilobite_decay, data1[:,0],data1[:,1], popt1)
+
+print 1e3*popt1[1]/(2.*np.pi)
+print 2.*np.pi/popt1[1]
 
 #fitRange = np.arange(0,15,0.01)
 #plt.plot(data1[:,0],(data1[:,1]),"o--",label=r'Data (store_001)')
 #plt.plot(fitRange,decay(fitRange, popt1[0], popt1[1], popt1[2], popt1[3]))
 
+txs=np.linspace(0,16,100)
 
 plot_dict={}
 
@@ -41,15 +51,17 @@ f=plt.figure()
 
 h=pd.DataFrame(index=data1[:,0],data=data1[:,1])
 h_err=pd.DataFrame(index=data1[:,0],data=data1[:,2])
-#h2=pd.DataFrame(index=ds,data=fitfunc(ds,*popt2))
+h2=pd.DataFrame(index=txs,data=trilobite_decay(txs,*popt1))
 
 
 plot_dict['111']={
-    'A':{'type':'errorbar','y':h[0].to_json(),'yerr':h_err[0].to_json(),'ylabel':u'Effizienz','xlabel':u'Speicherzeit ($\mu s$)','xlim':(0.,12.),'ylim':(0,0.25),'label':'Daten'}
+    'A':{'type':'errorbar','y':h[0].to_json(),'yerr':h_err[0].to_json(),'ylabel':u'Effizienz','xlabel':u'Speicherzeit ($\mu s$)','xlim':(0.,12.),'ylim':(0,0.25),'label':'Daten'},
+    'B':{'type':'plot','y':h[0].to_json()}
 }
 
 plt.subplot(111)
 plt.errorbar(data1[:,0], data1[:,1], yerr=data1[:,2], ls="", marker='o',lw=1.5,c='r')
+plt.plot(txs, trilobite_decay(txs,*popt1))
 
 
 #h=pd.DataFrame(index=densitys/1e18,data=phases[:,5])
@@ -67,8 +79,8 @@ plt.errorbar(data1[:,0], data1[:,1], yerr=data1[:,2], ls="", marker='o',lw=1.5,c
 
 
 
-with io.open('storage_retrieval.json', 'w+') as f:
-  f.write(unicode(json.dumps(plot_dict, ensure_ascii=False,indent=4)))
+#with io.open('storage_retrieval.json', 'w+') as f:
+#  f.write(unicode(json.dumps(plot_dict, ensure_ascii=False,indent=4)))
 
 
 plt.show()
---
+++ Chap5/trilobite_binding.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 12:27:15 2017
+
+@author: daniel
+"""
+
+import numpy as np
+import scipy.constants as const
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+import rydpy2 as rydpy
+
+Ry = 109736.605e2# * const.c #Rydberg constant (Hz) from [1]
+a0 = 0.52917721067e-10 # m, Bohr radius
+hbar = 6.626070040e-34/(2 * np.pi) # Js, Planck constant, CODATA 2014
+Eh=const.physical_constants["Hartree energy"][0]
+me=const.m_e
+u=const.physical_constants["atomic mass constant"][0]
+m=87*u
+
+
+
+bound=8910
+bound_u=150
+#for n in np.arange(69,70):
+r, rrRR, Vscat, G = rydpy.Numerov(69,0.,1./2)
+rV,Vs2,_=rydpy.ScatteringPotential(69,0.,1./2)
+
+V_min_r=np.argmin(Vs2[bound:])
+V_min=np.min(Vs2[bound:])
+
+V_frac=0.7
+
+VscatMin=Vs2[V_min_r+bound]
+iStart=V_min_r+bound-1
+while np.abs(Vs2[iStart]) > np.abs(VscatMin)*V_frac:
+    iStart=iStart-1
+
+print iStart
+iStop=V_min_r+bound+1
+while np.abs(Vs2[iStop]) > np.abs(VscatMin)*V_frac:
+    iStop=iStop+1
+
+print iStop
+
+off=V_min*Eh
+x0=rV[bound:][V_min_r]*a0
+
+def harmFunc(x, w):
+  return 1./2*m*w**2*(x-x0)**2+off
+
+
+
+popt1=[2*np.pi*400e3]
+popt1, pcov1 = curve_fit(harmFunc, rV[iStart:iStop]*a0,Vs2[iStart:iStop]*Eh, popt1)
+b_state = harmFunc(rV[iStart:iStop]*a0,*popt1)
+
+v0=popt1[0]/2+off/hbar
+
+print "tau: %f"%(-1*1e6*2*np.pi/(v0))
+print "omega: %f"%(v0/(2.*np.pi))
+
+fig=plt.figure(0)
+fig.clear()
+ax0=fig.add_subplot(211)
+ax1=fig.add_subplot(212)
+
+start=2000
+ax0.plot(r[start:], rrRR[start:]/(r[start:]*r[start:]))
+ax0.set_ylabel("$|r R_{n,l}|^2$")
+ax0.set_xlabel("$r/a_0$")
+
+#ax0.set_title("$%ds_{1/2}$" %n)
+
+ax1.plot(rV[5000:]*a0, (Vs2[5000:]*Eh/hbar/(2*np.pi))/1e3)
+ax1.plot(rV[iStart:iStop]*a0, (b_state/hbar/(2*np.pi))/1e3)
+ax1.axvline(rV[bound:][V_min_r]*a0)
+ax1.axhline((V_min*Eh/hbar/(2*np.pi))/1e3)
+#  ax1.set_ylim([-5.2,0])
+ax1.grid()
+#  ax1.plot(p[0], harmFunc(*p)*2*Ry/1e6, label=fitStr)
+ax1.legend(loc=4, prop={"size" : 9})
+  #ax1.set_xlim([500,2500])
+ax1.set_ylabel("$V_{scatt} (MHz)$")
+ax1.set_xlabel("$r/a_0$")
+fig.show()
+
+  #fig.set_size_inches((7,6))
+  #plt.savefig("example.pdf")
+
+  #f=open("theory.tsv", "a")
+  #f.write("%d\t%e\n" % (n, nu0))
+  #f.close()
